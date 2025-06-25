b252c1a60557c6f84272c6f8e0415c6e6e342676
minahilf <sminahilfatima@gmail.com>
2025-06-25 17:29:19 +0500

    update env

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -6,11 +6,11 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "to-do-list-fb6b3.firebaseapp.com",
-  projectId: "to-do-list-fb6b3",
-  storageBucket: "to-do-list-fb6b3.firebasestorage.app",
-  messagingSenderId: "1007312461219",
-  appId: "1:1007312461219:web:e620594b1cbad85b568e16"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
